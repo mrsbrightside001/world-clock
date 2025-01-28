eaa456672e77f4acf04aaf9cbd7d72320004cf46
mrsbrightside001 <155654171+mrsbrightside001@users.noreply.github.com>
2025-01-28 11:10:49 +0100

    Fix missing cities on initial refresh

--- src/app.js
+++ src/app.js
@@ -6,46 +6,44 @@ function updateTime() {
     let losAngelesTimeElement = losAngelesElement.querySelector(".time");
     let losAngelesTime = moment().tz("America/Los_Angeles");
 
-    losAngelesDateElement.innerHTML = losAngelesTime.format(
-      "dddd, MMMM Do, YYYY"
-    );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
     losAngelesTimeElement.innerHTML = losAngelesTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
-    // Prague
-    let pragueElement = document.querySelector("#prague");
-    if (pragueElement) {
-      let pragueDateElement = pragueElement.querySelector(".date");
-      let pragueTimeElement = pragueElement.querySelector(".time");
-      let pragueTime = moment().tz("Europe/Prague");
+  }
 
-      pragueDateElement.innerHTML = pragueTime.format("dddd, MMMM Do, YYYY");
-      pragueTimeElement.innerHTML = pragueTime.format(
-        "h:mm:ss [<small>]A[</small>]"
-      );
-    }
+  // Prague
+  let pragueElement = document.querySelector("#prague");
+  if (pragueElement) {
+    let pragueDateElement = pragueElement.querySelector(".date");
+    let pragueTimeElement = pragueElement.querySelector(".time");
+    let pragueTime = moment().tz("Europe/Prague");
+
+    pragueDateElement.innerHTML = pragueTime.format("MMMM	Do YYYY");
+    pragueTimeElement.innerHTML = pragueTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
   }
 }
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-  <div class="city" id="los-angeles">
+  <div class="city">
     <div>
       <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-      </div>
-      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small>
-      </div>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
   `;
-
-  console.log(cityTime.format("MMMM Do YYYY"));
 }
+
 updateTime();
 setInterval(updateTime, 1000);
 
